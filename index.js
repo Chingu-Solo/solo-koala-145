9d643fe18b5728d0d6625c448e3963849fc7d424
VitaliiLynnyk <linnik2101@gmail.com>
2020-02-14 16:28:19 +0100

    implemented typeFontsText funct

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+let data = [...initialArr];
+
 const searchFont = document.querySelector(".searchFont");
-searchFont.addEventListener("input", searchFontByName);
+searchFont.addEventListener("input", e => searchFontByName(e, data, ".articles-res"));
 
-initialArr.forEach(el => createArticleTemplate(el, ".articles-res"));
+const typeSmth = document.querySelector(".toolbar-text");
+typeSmth.addEventListener("input", e => typeFontsText(e, data, ".articles-res"));
 
-function createArticleTemplate(item, className) {
+data.forEach(el => createArticleTemplate(el, ".articles-res"));
+
+function createArticleTemplate(item, parentClass) {
   //classStyle, nameStyle, authorStyle, text
   
   const article = document.createElement("article");
@@ -33,16 +38,24 @@ function createArticleTemplate(item, className) {
   article.appendChild(h3);
   article.appendChild(p);
   
-  const parent = document.querySelector(className);
+  const parent = document.querySelector(parentClass);
   parent.appendChild(article);
 }
 
-function searchFontByName(event) {
+function searchFontByName(event, data, parentClass) {
   const { target: { value }} = event;
-  const filteredArr = initialArr.filter(e => e.nameStyle.toLowerCase().indexOf(value) >= 0);
+  const filteredArr = data.filter(e => e.nameStyle.toLowerCase().indexOf(value) >= 0);
   
-  removeAllChildren('.articles-res');
-  filteredArr.forEach(el => createArticleTemplate(el, ".articles-res"));
+  removeAllChildren(parentClass);
+  filteredArr.forEach(el => createArticleTemplate(el, parentClass));
+}
+
+function typeFontsText(event, data, parentClass) {
+  const { target: { value }} = event;
+  const newArray = data.map(el => Object.assign({}, el, {["text"]: value}));
+  
+  removeAllChildren(parentClass);
+  newArray.forEach(el => createArticleTemplate(el, parentClass));
 }
 
 function removeAllChildren(className) {
